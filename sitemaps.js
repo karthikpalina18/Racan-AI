f779adac62a52c8c9ab51836d75404ebd7f481fc
brahmarishendra <brahmarishi868@gmail.com>
2025-05-31 15:36:24 +0530

    Sitemaps

---
+++ sitemaps.js
@@ -0,0 +1,15 @@
+const { SitemapStream, streamToPromise } = require('sitemap');
+const { createWriteStream } = require('fs');
+
+const sitemap = new SitemapStream({ hostname: 'https://yourdomain.com' });
+
+sitemap.write({ url: '/', changefreq: 'daily', priority: 1.0 });
+sitemap.write({ url: '/about', changefreq: 'monthly', priority: 0.8 });
+sitemap.write({ url: '/contact', changefreq: 'monthly', priority: 0.7 });
+
+sitemap.end();
+
+streamToPromise(sitemap)
+  .then(data => {
+    createWriteStream('./public/sitemap.xml').end(data);
+  });
